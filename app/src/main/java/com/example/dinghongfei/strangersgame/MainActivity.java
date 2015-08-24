9254e343180184edd33d73f2e9a77d10991c767f
lannka <hungfei.ding@gmail.com>
2015-08-24 15:46:06 -0700

    Check bluetooth status on the device and prompt msg if it's off.

--- app/src/main/java/com/example/dinghongfei/strangersgame/MainActivity.java
+++ app/src/main/java/com/example/dinghongfei/strangersgame/MainActivity.java
@@ -1,37 +1,89 @@
 package com.example.dinghongfei.strangersgame;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  private static final int REQUEST_ENABLE_BLUETOOTH = 1;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    init();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    return super.onOptionsItemSelected(item);
+  }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+      if (resultCode == Activity.RESULT_OK) {
+        init();
+      } else {
+        finish();
+      }
     }
+  }
+
+  // Checks if Bluetooth advertising is supported on the device and requests enabling if necessary.
+  private void init() {
+    BluetoothManager manager = (BluetoothManager) getApplicationContext().getSystemService(
+        Context.BLUETOOTH_SERVICE);
+    BluetoothAdapter btAdapter = manager.getAdapter();
+    if (btAdapter == null) {
+      showFinishingAlertDialog("Bluetooth Error", "Bluetooth not detected on device");
+    } else if (!btAdapter.isEnabled()) {
+      Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+      this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+    } else if (!btAdapter.isMultipleAdvertisementSupported()) {
+      showFinishingAlertDialog("Not supported", "BLE advertising not supported on this device");
+    } else {
+      BluetoothLeAdvertiser adv = btAdapter.getBluetoothLeAdvertiser();
+    }
+  }
+
+  // Pops an AlertDialog that quits the app on OK.
+  private void showFinishingAlertDialog(String title, String message) {
+    new AlertDialog.Builder(this)
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            finish();
+          }
+        }).show();
+  }
 }
