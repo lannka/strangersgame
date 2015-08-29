f10a2e7ce0069809482c904dfa1c3c904393889f
lannka <hungfei.ding@gmail.com>
2015-08-29 11:58:33 -0700

    Simplify the game logic a little bit by removing one thread.

--- app/src/main/java/com/example/dinghongfei/strangersgame/GameController.java
+++ app/src/main/java/com/example/dinghongfei/strangersgame/GameController.java
@@ -14,14 +14,14 @@ import java.util.Set;
 
 public class GameController {
 
+  private static final int RESPONSE_INTERVAL_IN_MS = 500;
+  private static final int FULL_LIFE_IN_SEC = 300;
+
   private final Activity context;
   private String myBaseId;
   private String enemyBaseId;
   private Set<String> enemyIds;
   private Vibrator vibrator;
-  private long last_found_enemy_time;
-  private long last_found_self_base_time;
-  private long last_found_enemy_base_time;
   private boolean found_enemy = false;
   private boolean found_self_base = false;
   private boolean found_enemy_base = false;
@@ -42,30 +42,6 @@ public class GameController {
   public GameController(Activity context) {
     this.context = context;
     vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-    new Thread(new Runnable() {
-      public void run() {
-          while (true) {
-              Date date = new Date();
-              long current_time = date.getTime();
-              found_enemy = (current_time - last_found_enemy_time) < 1000;
-              found_enemy_base = (current_time - last_found_enemy_base_time) < 1000;
-              found_self_base = (current_time - last_found_self_base_time) < 1000;
-
-              if (!found_self_base && game_started) {
-                  life -= 1;
-              }
-
-              try {
-                  Thread.sleep(1000);
-              } catch (InterruptedException e) {
-                  e.printStackTrace();
-              }
-              Log.i("GameStatus",
-                      "found_enemy: " + found_enemy + "  found_enemy_base: " + found_enemy_base +
-                              "  found_self_base: " + found_self_base);
-          }
-      }
-    }).start();
   }
 
   public void start(String myBaseId, String enemyBaseId, Set<String> enemyIds) {
@@ -78,10 +54,7 @@ public class GameController {
     found_enemy_text = (TextView) (context.findViewById(R.id.found_enemy_label));
     found_enemy_base_text = (TextView) (context.findViewById(R.id.found_enemy_base_label));
     progressBar = (ProgressBar) (context.findViewById(R.id.circle_progress_bar));
-    ResetLife();
-    last_found_enemy_time = -1;
-    last_found_self_base_time = -1;
-    last_found_enemy_time = -1;
+    life = FULL_LIFE_IN_SEC;
     progress = 0;
     progressBar.setProgress(0);
     progressBar.setMax(100);
@@ -100,7 +73,7 @@ public class GameController {
       public void run() {
         while (!Thread.currentThread().isInterrupted()) {
           try {
-            Thread.sleep(100);
+            Thread.sleep(RESPONSE_INTERVAL_IN_MS);
           } catch (InterruptedException e) {
             e.printStackTrace();
           }
@@ -109,22 +82,26 @@ public class GameController {
               if (!game_started) {
                 return;
               }
-              if (found_self_base && !prev_found_self_base) {
-                count_down_timer.setText("");
-                progressBar.setVisibility(View.VISIBLE);
-                progressBarBackground.setVisibility(View.VISIBLE);
-                progress = 0;
-                progressBar.setProgress(0);
-                prev_found_self_base = true;
-              } else if (found_self_base && prev_found_self_base) {
-                count_down_timer.setText("");
-                if (progress < 100) {
-                  progress += 1;
-                  progressBar.setProgress(progress);
+              if (found_self_base) {
+                if (!prev_found_self_base) {
+                  count_down_timer.setText("");
+                  progressBar.setVisibility(View.VISIBLE);
+                  progressBarBackground.setVisibility(View.VISIBLE);
+                  progress = 0;
+                  progressBar.setProgress(0);
+                  prev_found_self_base = true;
+                  found_self_base = false;
                 } else {
-                  ResetLife();
+                  count_down_timer.setText("");
+                  found_self_base = false;
+                  if (progress < 100) {
+                    progress += RESPONSE_INTERVAL_IN_MS / 100;
+                    progressBar.setProgress(progress);
+                  } else {
+                    life = FULL_LIFE_IN_SEC;
+                  }
                 }
-              } else if (!found_self_base) {
+              } else {
                 if (prev_found_self_base) {
                   prev_found_self_base = false;
                   progressBar.setVisibility(View.GONE);
@@ -143,11 +120,13 @@ public class GameController {
 
               if (found_enemy) {
                 found_enemy_text.setText("Enemy Around!!!");
+                found_enemy = false;
               } else {
                 found_enemy_text.setText("");
               }
               if (found_enemy_base) {
                 found_enemy_base_text.setText("Found Enemy Base!!!");
+                found_enemy_base = false;
               } else {
                 found_enemy_base_text.setText("");
               }
@@ -162,19 +141,15 @@ public class GameController {
   public void interrupt(String instanceId) {
     Date date = new Date();
     if (instanceId.equals(myBaseId)) {
-      last_found_self_base_time = date.getTime();
+      found_self_base = true;
       Log.i("Detected", "My base in range: " + myBaseId + "at time: " + date.getTime());
     } else if (instanceId.equals(enemyBaseId)) {
-      last_found_enemy_base_time = date.getTime();
+      found_enemy_base = true;
       Log.i("Detected", "Enemy base in range: " + enemyBaseId + "at time: " + date.getTime());
     } else if (enemyIds.contains(instanceId)) {
-      last_found_enemy_time = date.getTime();
+      found_enemy = true;
       Log.i("Detected", "Enemy in range: " + instanceId + "at time: " + date.getTime());
       vibrator.vibrate(200);
     }
-  }
-
-  public void ResetLife() {
-      life = 300;
   }
 }
