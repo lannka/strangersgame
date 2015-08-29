205f1d117f55a29e143e2dbce8782a7a53746a5c
lannka <hungfei.ding@gmail.com>
2015-08-28 18:00:58 -0700

    Moves game logic into GameController

--- app/src/main/java/com/example/dinghongfei/strangersgame/GameController.java
+++ app/src/main/java/com/example/dinghongfei/strangersgame/GameController.java
@@ -1,29 +1,45 @@
 package com.example.dinghongfei.strangersgame;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.Date;
 import java.util.Set;
 
 public class GameController {
 
-  private final Context context;
+  private final Activity context;
   private String myBaseId;
   private String enemyBaseId;
   private Set<String> enemyIds;
   private Vibrator vibrator;
-  private long last_found_enemy_time = -1;
-  private long last_found_self_base_time = -1;
-  private long last_found_enemy_base_time = -1;
-  public boolean found_enemy = false;
-  public boolean found_self_base = false;
-  public boolean found_enemy_base = false;
-  public int life = 300;  // 10 HP = 1 second
-  public boolean game_started = false;
+  private long last_found_enemy_time;
+  private long last_found_self_base_time;
+  private long last_found_enemy_base_time;
+  private boolean found_enemy = false;
+  private boolean found_self_base = false;
+  private boolean found_enemy_base = false;
+  private int life;
+  private boolean game_started = false;
 
-  public GameController(Context context) {
+  private ProgressBar progressBar;
+  private ProgressBar progressBarBackground;
+  private int progress;
+  private Handler handler;
+  private boolean prev_found_self_base = false;
+  private TextView found_enemy_text;
+  private TextView found_enemy_base_text;
+  private TextView count_down_timer;
+
+  private Thread timerThread;
+
+  public GameController(Activity context) {
     this.context = context;
     vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     new Thread(new Runnable() {
@@ -58,6 +74,89 @@ public class GameController {
     this.enemyIds = enemyIds;
     game_started = true;
     Log.i("Start game", myBaseId + ":" + enemyBaseId + ":" + enemyIds);
+
+    found_enemy_text = (TextView) (context.findViewById(R.id.found_enemy_label));
+    found_enemy_base_text = (TextView) (context.findViewById(R.id.found_enemy_base_label));
+    progressBar = (ProgressBar) (context.findViewById(R.id.circle_progress_bar));
+    ResetLife();
+    last_found_enemy_time = -1;
+    last_found_self_base_time = -1;
+    last_found_enemy_time = -1;
+    progress = 0;
+    progressBar.setProgress(0);
+    progressBar.setMax(100);
+    progressBar.setVisibility(View.GONE);
+    progressBarBackground = (ProgressBar) (context.findViewById(R.id.circle_progress_bar_background));
+    progressBarBackground.setVisibility(View.GONE);
+    count_down_timer = (TextView) (context.findViewById(R.id.timer));
+    count_down_timer.setText("");
+    handler = new Handler();
+
+    if (timerThread != null) {
+      timerThread.interrupt();
+      timerThread = null;
+    }
+    timerThread = new Thread(new Runnable() {
+      public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          handler.post(new Runnable() {
+            public void run() {
+              if (!game_started) {
+                return;
+              }
+              if (found_self_base && !prev_found_self_base) {
+                count_down_timer.setText("");
+                progressBar.setVisibility(View.VISIBLE);
+                progressBarBackground.setVisibility(View.VISIBLE);
+                progress = 0;
+                progressBar.setProgress(0);
+                prev_found_self_base = true;
+              } else if (found_self_base && prev_found_self_base) {
+                count_down_timer.setText("");
+                if (progress < 100) {
+                  progress += 1;
+                  progressBar.setProgress(progress);
+                } else {
+                  ResetLife();
+                }
+              } else if (!found_self_base) {
+                if (prev_found_self_base) {
+                  prev_found_self_base = false;
+                  progressBar.setVisibility(View.GONE);
+                  progressBarBackground.setVisibility(View.GONE);
+                  progressBar.setProgress(0);
+                  progress = 0;
+                }
+                if (life < 0) {
+                  count_down_timer.setText("Game Over! You Lose!");
+                } else {
+                  int sec = life % 60;
+                  int min = life / 60;
+                  count_down_timer.setText(Integer.toString(min) + ":" + Integer.toString(sec));
+                }
+              }
+
+              if (found_enemy) {
+                found_enemy_text.setText("Enemy Around!!!");
+              } else {
+                found_enemy_text.setText("");
+              }
+              if (found_enemy_base) {
+                found_enemy_base_text.setText("Found Enemy Base!!!");
+              } else {
+                found_enemy_base_text.setText("");
+              }
+            }
+          });
+        }
+      }
+    });
+    timerThread.start();
   }
 
   public void interrupt(String instanceId) {
